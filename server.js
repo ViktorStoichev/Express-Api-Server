73b11def242b95b6774b3346969217b9e7030e36
Viktor Stoichev <viktor.stoichev534@gmail.com>
2025-06-19 22:29:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middlewares
 app.use(cors());
-app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/smartswap', smartswapRoutes);
