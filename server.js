d264bf98bcf678b1d0fbfe3d4dc36e017296691d
Viktor Stoichev <viktor.stoichev534@gmail.com>
2025-06-19 21:03:09 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import smartswapRoutes from './routes/smartswap.routes.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/smartswap', smartswapRoutes);
+
+// Base route
+app.get('/', (req, res) => {
+  res.send('Shared API Server is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
