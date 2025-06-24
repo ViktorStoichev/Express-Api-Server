decffd37479f27d9a0deaca68196803a8a850a43
Viktor Stoichev <viktor.stoichev534@gmail.com>
2025-06-24 19:17:56 +0300

    Update cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,4 +1,7 @@
 import cloudinary from 'cloudinary';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 console.log(process.env.CLOUDINARY_CLOUD_NAME);
 console.log(process.env.CLOUDINARY_API_KEY);
