a850b4cae113e5d51cf096c72408d1e6b5825de9
Viktor Stoichev <viktor.stoichev534@gmail.com>
2025-06-24 19:15:46 +0300

    Test changes

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import cors from 'cors';
-
+import dotenv from 'dotenv';
 import smartswapRoutes from './routes/smartswap.routes.js';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
