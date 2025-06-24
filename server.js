df3becf003dcfd22d002297d2ae89e8b165a839d
Viktor Stoichev <viktor.stoichev534@gmail.com>
2025-06-24 18:59:23 +0300

    Update environments

--- server.js
+++ server.js
@@ -1,10 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 
 import smartswapRoutes from './routes/smartswap.routes.js';
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
