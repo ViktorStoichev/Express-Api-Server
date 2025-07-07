873e67976c4b1ec36f9b621279d464c961143c88
ViktorStoichev <viktor.stoichev888@abv.bg>
2025-07-07 14:31:53 +0300

    Update coinscope.routes.js

--- routes/coinscope.routes.js
+++ routes/coinscope.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAllCoins, getOneCoin } from "../controllers/coinscope.controller";
+import { getAllCoins, getOneCoin } from "../controllers/coinscope.controller.js";
 
 const router = express.Router();
 
